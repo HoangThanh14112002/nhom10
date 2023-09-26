ace6e6f9f8e613d5d04e65df08f4f88086ffd1c8
hoangcao337 <145979592+hoangcao337@users.noreply.github.com>
2023-09-26 15:36:59 +0700

    Update thong_ke.py
    cao hoàng

--- thong_ke.py
+++ thong_ke.py
@@ -1,61 +1,62 @@
 import pandas as pd
-from numpy import array
-import matplotlib.pyplot as plt
 import numpy as np
-import tkinter as tk
-from tkinter import ttk
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
+from tkinter import scrolledtext
 
-# Đọc dữ liệu từ tệp CSV
-df = pd.read_csv('diemPython.csv', index_col=0, header=0)
-in_data = array(df.iloc[:, :])
 
-# Tạo cửa sổ chính và đặt kích thước
-root = tk.Tk()
-root.title("Ứng dụng Thông tin và Biểu đồ")
-root.geometry("800x600")  # Đặt kích thước cửa sổ là 800x600 pixel
+def run_code():
+    # Đọc dữ liệu từ file CSV
+    df = pd.read_csv('diemPython.csv', index_col=0, header=0)
 
-# Tạo hàm để hiển thị thông tin sinh viên
-def hien_thi_thong_tin():
-    info_window = tk.Toplevel(root)
-    info_window.title("Thông tin sinh viên")
+    # Chuyển DataFrame thành mảng numpy
+    in_data = np.array(df.iloc[:, :])
 
-    tongsv = np.sum(in_data[:, 1])
-    diemA = sum(in_data[:, 3])
-    diemF = sum(in_data[:, 10])
-    chuanL1 = sum(in_data[:, 11])
-    chuanL2 = sum(in_data[:, 12])
+    # Tính tổng số sinh viên đi thi
+    tong_sv = in_data[:, 1].sum()
+    result_text.insert(tk.INSERT, f'Tổng số sinh viên đi thi: {tong_sv}\n')
 
-    info_label = tk.Label(info_window, text=f"Tổng số sinh viên đi thi: {tongsv}\n"
-                                           f"Tổng số sinh viên đạt điểm A: {diemA}\n"
-                                           f"Tổng số sinh viên đạt điểm F: {diemF}\n"
-                                           f"Tổng số sinh viên đạt chuẩn L1: {chuanL1}\n"
-                                           f"Tổng số sinh viên đạt chuẩn L2: {chuanL2}\n")
-    info_label.pack()
+    # Lấy dữ liệu điểm A+ đến F
+    diem = {'A+': 2, 'A': 3, 'B+': 4, 'B': 5, 'C+': 6, 'C': 7, 'D+': 8, 'D': 9, 'F': 10}
+    for grade, column in diem.items():
+        diem_grade = in_data[:, column]
+        max_diem_grade = diem_grade.max()
+        lop_co_nhieu_diem_grade_nhat = np.where(diem_grade == max_diem_grade)[0]
+        result_text.insert(tk.INSERT,
+                           f'Lớp có nhiều điểm {grade} nhất là {in_data[lop_co_nhieu_diem_grade_nhat, 0]} với {max_diem_grade} sinh viên đạt điểm {grade}\n')
 
-# Tạo hàm để hiển thị biểu đồ
-def hien_thi_bieu_do():
-    chart_window = tk.Toplevel(root)
-    chart_window.title("Biểu đồ")
+    # Tính tổng số sinh viên đạt từng loại điểm A+, A, B+, B, C+, C, D+, D, F
+    for grade, column in diem.items():
+        tong_diem_grade = in_data[:, column].sum()
+        result_text.insert(tk.INSERT, f'Tổng số sinh viên đạt điểm {grade}: {tong_diem_grade}\n')
 
-    diemA = in_data[:, 3]
-    diemF = in_data[:, 10]
-
-    fig, ax = plt.subplots(figsize=(8, 6))  # Đặt kích thước biểu đồ là 800x600 pixel
-    ax.plot(range(len(diemA)), diemA, 'r-', label="Diem A")
-    ax.plot(range(len(diemF)), diemF, 'g-', label="Diem F")
-    ax.set_xlabel('Lơp')
-    ax.set_ylabel('So sv dat diem')
+    # Vẽ biểu đồ
+    fig = plt.Figure(figsize=(5, 4), dpi=100)
+    ax = fig.add_subplot(111)
+    for grade, column in diem.items():
+        ax.plot(range(len(in_data[:, column])), in_data[:, column], label=f"Điểm {grade}")
+    ax.set_xlabel('Lớp')
+    ax.set_ylabel('Số sv đạt điểm')
     ax.legend(loc='upper right')
 
-    canvas = FigureCanvasTkAgg(fig, master=chart_window)
-    canvas.get_tk_widget().pack()
+    # Hiển thị biểu đồ trên giao diện tkinter
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-# Tạo các nút để chuyển đổi giữa cửa sổ thông tin và cửa sổ biểu đồ
-button_thong_tin = ttk.Button(root, text="Thông tin sinh viên", command=hien_thi_thong_tin)
-button_thong_tin.pack()
 
-button_bieu_do = ttk.Button(root, text="Biểu đồ", command=hien_thi_bieu_do)
-button_bieu_do.pack()
+# Tạo giao diện tkinter
+root = tk.Tk()
+root.title("Ứng dụng Thống kê và Biểu đồ")
+root.geometry("800x600")  # Đặt kích thước cửa sổ là 800x600 pixel
+
+# Tạo khung văn bản cuộn để hiển thị kết quả
+result_text = scrolledtext.ScrolledText(root, wrap=tk.WORD)
+result_text.pack(fill=tk.BOTH, expand=True)
+
+# Tạo các nút để chạy mã và hiển thị thông tin
+button_run = tk.Button(root, text="Run Code", command=run_code)
+button_run.pack()
 
 root.mainloop()
