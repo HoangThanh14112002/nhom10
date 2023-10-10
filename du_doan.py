ea7cf17db5439f52982aeeaa17ada455d81f71a9
hoanxuan2002 <145457874+hoanxuan2002@users.noreply.github.com>
2023-10-10 15:48:32 +0700

    Update du_doan.py

--- du_doan.py
+++ du_doan.py
@@ -1,102 +1,84 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow.compat.v1 as tf
-import pandas as pd
-from numpy import array
-import datetime
+import tensorflow as tf
 
-tf.disable_v2_behavior()
-
-# Generating random linear data
-# There will be 50 data points ranging from 0 to 50
+# Tạo tập giá trị x và y
 x = np.linspace(0, 50, 50)
 y = np.linspace(0, 50, 50)
 
-# Add random noise to the x and y data to create random dataset
+# Cộng thêm nhiễu cho tập x và y để có tập dữ liệu ngẫu nhiên
 x += np.random.uniform(-4, 4, 50)
 y += np.random.uniform(-4, 4, 50)
-n = len(x)  # Number data points
+n = len(x)  # Số lượng dữ liệu
 
-# Plot of Training Data
+# Plot dữ liệu huấn luyện
 plt.scatter(x, y)
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title("Training Data")
+plt.title("Dữ liệu Huấn luyện")
 plt.show()
 
-# Creating model for the dataset
-X = tf.placeholder("float")
-Y = tf.placeholder("float")
-W = tf.Variable(np.random.randn(), name="W")
-b = tf.Variable(np.random.randn(), name="b")
+# Tạo model cho tập dữ liệu
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
 
+# Khởi tạo biến w và b
+W = tf.Variable(tf.random.normal([1]), name="W")
+b = tf.Variable(tf.random.normal([1]), name="b")
+
+# Thiết lập tốc độ học
 learning_rate = 0.01
+
+# Số vòng lặp
 training_epochs = 100
 
+# Hàm tuyến tính
 y_pred = tf.add(tf.multiply(X, W), b)
+
+# Mean Squared Error Cost Function
 cost = tf.reduce_sum(tf.pow(y_pred - Y, 2)) / (2 * n)
+
+# Tối ưu bằng Gradient Descent
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
-init = tf.global_variables_initializer()
-
+# Starting the TensorFlow Session
 with tf.Session() as sess:
-    sess.run(init)
+    # Khởi tạo các biến
+    sess.run(tf.global_variables_initializer())
 
+    # Iterating through all the epochs
     for epoch in range(training_epochs):
+        # Feeding each data point into the optimizer using Feed Dictionary
         for (_x, _y) in zip(x, y):
             sess.run(optimizer, feed_dict={X: _x, Y: _y})
 
+        # Displaying the result after every 50 epochs
         if (epoch + 1) % 50 == 0:
+            # Calculating the cost at every epoch
             c = sess.run(cost, feed_dict={X: x, Y: y})
             print("Epoch", (epoch + 1), ": cost =", c, "W =", sess.run(W), "b =", sess.run(b))
 
+    # Lấy các giá trị cần thiết để sử dụng ngoài Session
     training_cost = sess.run(cost, feed_dict={X: x, Y: y})
-    weight = sess.run(W)
-    bias = sess.run(b)
+    weight = sess.run(W)[0]
+    bias = sess.run(b)[0]
 
-    # Calculating the predictions
-    predictions = weight * x + bias
-    print("Training cost =", training_cost, "Weight =", weight, "bias =", bias, '\n')
+# Lưu mô hình đã huấn luyện bằng Pickle
+model = {'weight': weight, 'bias': bias}
+with open('linear_model.pkl', 'wb') as model_file:
+    pickle.dump(model, model_file)
 
-    # Calculate Mean Squared Error (MSE)
-    mse = np.mean((predictions - y) ** 2)
-    print("Mean Squared Error (MSE) =", mse)
+# Đọc mô hình từ tệp và sử dụng nó để dự đoán dữ liệu mới
+with open('linear_model.pkl', 'rb') as model_file:
+    loaded_model = pickle.load(model_file)
 
-    # Plotting the Results
-    plt.scatter(x, y, label='Original data')
-    plt.plot(x, predictions, label='Fitted line')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('Linear Regression Result')
-    plt.legend()
-    plt.show()
+new_x = np.array([55, 60, 65, 70])  # Dữ liệu mới
+new_predictions = loaded_model['weight'] * new_x + loaded_model['bias']
+print("Dự đoán cho dữ liệu mới:", new_predictions)
 
-# Generating random linear test data
-# There will be 20 data points ranging from 0 to 50
-x_test = np.linspace(0, 50, 20)
-y_test = np.linspace(0, 50, 20)
-
-# Add random noise to the x and y test data to create random dataset
-x_test += np.random.uniform(-4, 4, 20)
-y_test += np.random.uniform(-4, 4, 20)
-
-# Calculate predictions for test data
-predictions_test = weight * x_test + bias
-
-# Plotting the Results for test data
-plt.scatter(x_test, y_test, label='Original test data')
-plt.plot(x_test, predictions_test, label='Fitted line')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Linear Regression Test Result')
+# Plot kết quả
+plt.plot(x, y, 'ro', label='Dữ liệu gốc')
+plt.plot(new_x, new_predictions, 'bo', label='Dự đoán cho dữ liệu mới')
+plt.title('Kết quả Hồi quy Tuyến tính')
 plt.legend()
 plt.show()
-
-# Print current date and time for prediction
-now = datetime.datetime.now()
-print("Current date and time is:", now)
-
-# Predict for new values
-x_new = np.array([10, 20, 30, 40, 50])
-predictions_new = weight * x_new + bias
-print("Predictions for new values:", predictions_new)
