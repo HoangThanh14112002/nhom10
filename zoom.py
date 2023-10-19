bfaa8237055656148c29af5f15734597336c8a93
hoangcao337 <145979592+hoangcao337@users.noreply.github.com>
2023-10-19 14:00:13 +0700

    Hoang commit

--- zoom.py
+++ zoom.py
@@ -39,21 +39,23 @@ scale_factor_y_label = ttk.Label(window, text="Tỷ lệ Y:")
 scale_factor_y_label.grid(row=1, column=0)
 scale_factor_y_entry = ttk.Entry(window)
 scale_factor_y_entry.grid(row=1, column=1)
-def zoom_button_clicked():
-    scale_factor_x = float(scale_factor_x_entry.get())
-    scale_factor_y = float(scale_factor_y_entry.get())
-    zoom_image(scale_factor_x, scale_factor_y)
-zoom_button = ttk.Button(window, text="Zoom", command=zoom_button_clicked)
+
+zoom_button = ttk.Button(window, text="Zoom", command=lambda: zoom_button_clicked())
 zoom_button.place(x=20, y=20)  # Điều chỉnh vị trí (x, y) của nút "Zoom" theo ý muốn
 
 # Nút Reset
 reset_button = ttk.Button(window, text="Reset", command=reset_image)
 reset_button.place(x=100, y=20)  # Điều chỉnh vị trí (x, y) của nút "Reset" theo ý muốn
+
 # Hiển thị ảnh ban đầu
 photo_label = ttk.Label(window)
 display_image(original_image)
 photo_label.grid(row=2, columnspan=4)
 
 # Hàm xử lý sự kiện khi nhấn nút Zoom
+def zoom_button_clicked():
+    scale_factor_x = float(scale_factor_x_entry.get())
+    scale_factor_y = float(scale_factor_y_entry.get())
+    zoom_image(scale_factor_x, scale_factor_y)
 
 window.mainloop()
